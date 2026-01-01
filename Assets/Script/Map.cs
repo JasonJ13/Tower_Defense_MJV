794a311c67943bde8f2bdd837fc68b6c4019281c
Ezloz <95761894+Ezloz@users.noreply.github.com>
2026-01-01 17:45:43 +0100

    You can now load mapArray from png
    Visuals are missing but MapArray is working. Adding visual next

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.IO;
 
 public class MapLoader : MonoBehaviour
 {
@@ -11,11 +12,8 @@ public class MapLoader : MonoBehaviour
         constructible
     }
 
-
-    [SerializeField]
-    private TileType[,] mapArray;
 /*
-(299, 229, 229) Zone non constructible = 0 Tile asset : snow-tile
+(229, 229, 229) Zone non constructible = 0 Tile asset : snow-tile
 (255, 233, 127) Chemin = 1 Tile asset : snow-tile-straight/square
 (255, 178, 127) intersection = 2 Tile asset : snow-tile-split/crossing
 (0, 255, 33) Entree = 3 Tile asset : snow-tile-end
@@ -23,17 +21,45 @@ public class MapLoader : MonoBehaviour
 other Zone constructible = 5 Tile asset : snow-tile-hill
 */
 
+    private Color32 colorEmpty = new Color32(229, 229, 229, 255);
+    private Color32 colorRoad = new Color32(255, 233, 127, 255);
+    private Color32 colorCross = new Color32(255, 178, 127, 255);
+    private Color32 colorStart = new Color32(0, 255, 33, 255);
+    private Color32 colorEnd = new Color32(255, 0, 0, 255);
+
+    [SerializeField]
+    private GameObject TileEmpty; // visual of the tile 
+    [SerializeField]
+    private GameObject TileRoad1;
+    [SerializeField]
+    private GameObject TileRoad2;
+    [SerializeField]
+    private GameObject TileCross1;
+    [SerializeField]
+    private GameObject TileCross2;
+    [SerializeField]
+    private GameObject TileStart;
+    [SerializeField]
+    private GameObject TileEnd;
+
+
+    private TileType[,] mapArray; // 2DArray representation of a map
+
     private TileType[,] mapArcs;
 
-
     private int[,] mapTruc;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        string mapDiskPath = Application.dataPath + "/Maps/map_01.png";
         //A faire : Agresser l'utilisateur à choisir une map
         // Convertir la map en mapArray
         //faire d'autre trucs
+
+        mapArray = LoadMapArray(mapDiskPath);
+        Debug.Log(mapArray);
 
         TileType[,] mapArrayTest = { //map Test
                 {TileType.empty, TileType.empty, TileType.end, TileType.empty, TileType.empty},
@@ -41,7 +67,7 @@ other Zone constructible = 5 Tile asset : snow-tile-hill
                 {TileType.start, TileType.road, TileType.cross, TileType.end, TileType.empty},
                 {TileType.empty, TileType.constructible, TileType.empty, TileType.empty, TileType.empty},
                 {TileType.empty, TileType.empty, TileType.empty, TileType.empty, TileType.empty}
-        };
+                };
 
         mapArray = mapArrayTest;
     }
@@ -51,4 +77,80 @@ other Zone constructible = 5 Tile asset : snow-tile-hill
     {
 
     }
+
+    private TileType[,] LoadMapArray(string path){ // create the MapArray associated to the png given path.
+
+        Texture2D mapImage = new Texture2D(2, 2);
+        mapImage.LoadImage(File.ReadAllBytes(path));
+
+        TileType[,] mapArray = new TileType[mapImage.height, mapImage.width];
+
+        Color32[] pixels = mapImage.GetPixels32();
+        for (int y = 0; y < mapImage.height; y++)
+        {
+            for (int x = 0; x < mapImage.width; x++)
+            {
+                Color32 pixel = pixels[y * mapImage.width + (mapImage.width-1-x)]; // VA SAVOIR POURQUOI mais juste "+ x" ça lis de droite à gauche .-.
+
+                if (pixel.Equals(colorEmpty)){
+                   mapArray[y,x] = TileType.empty;
+                }
+                else if (pixel.Equals(colorRoad)){
+                   mapArray[y,x] = TileType.road;
+                }
+                else if (pixel.Equals(colorCross)){
+                   mapArray[y,x] = TileType.cross;
+                }
+                else if (pixel.Equals(colorStart)){
+                   mapArray[y,x] = TileType.start;
+                }
+                else if (pixel.Equals(colorEnd)){
+                   mapArray[y,x] = TileType.end;
+                }
+                else{
+                   mapArray[y,x] = TileType.constructible;
+                }
+
+
+            }
+        }
+
+
+        return mapArray;
+    }
+
+    private void LoadTiles(TileType[,] mapArray){ // create the Tiles associated to the MapArray .
+
+        for (int y = 0; y < mapArray.GetLength(0); y++)
+        {
+            for (int x = 0; x < mapArray.GetLength(1); x++)
+            {
+                if (mapArray[y,x] == TileType.empty){
+                   Instantiate(TileEmpty);
+                }
+                else if (mapArray[y,x] == TileType.road){
+                    
+                   
+                }
+                else if (mapArray[y,x] == TileType.cross){
+                   
+                }
+                else if (mapArray[y,x] == TileType.start){
+                   
+                }
+                else if (mapArray[y,x] == TileType.end){
+                   
+                }
+                else{
+                   mapArray[y,x] = TileType.constructible;
+                }                
+            }
+        }
+
+
+    }
+
+
+
+
 }
