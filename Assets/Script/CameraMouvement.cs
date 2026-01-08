2e28818542d8824b751e7f1a67fd12377e9ac39f
JasonJ13 <jason.jallabert@orange.fr>
2026-01-08 12:24:17 +0100

    sans erreur

--- Assets/Script/CameraMouvement.cs
+++ Assets/Script/CameraMouvement.cs
@@ -86,11 +86,11 @@ public class CameraMouvement : MonoBehaviour
             Vector2 mousePosition = Mouse.current.position.ReadValue();
             Ray ray = cameraComponent.ScreenPointToRay(mousePosition);
 
-            var interactible = hitInfo.collider.GetComponent<InteractibleObject>();
+            /*var interactible = hitInfo.collider.GetComponent<InteractibleObject>();
             if (interactible != null)
             {
                 Debug.Log(interactible.name);
-            }
+            }*/
         }
     }
 }
