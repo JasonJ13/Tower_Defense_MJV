ecc9504d14ac2dc30989734d86eef7aed19a59e6
Ezloz <95761894+Ezloz@users.noreply.github.com>
2026-01-22 17:03:08 +0100

    IMPORTANT STRUCTURAL CHANGE
    - Created a Graph class inside Map since the graph part of it became increasingly more complex
    - moved a lot of function/structs inside it in consequences
    -Created a node struct (experimental)
    -Added GetGraph() in Map to get the graph of the map

--- Assets/Script/Map.cs
+++ Assets/Script/Map.cs
@@ -47,31 +47,6 @@ public class Map : MonoBehaviour
       }
    }
 
-   /// <summary>
-   /// used for listing edges in mapArrayGraphAdj
-   /// </summary>
-   public struct edge 
-   {
-      public coords node1;
-      public coords node2;
-
-      public int weight;
-      public edge(coords node1, coords node2, int weight)
-      {
-         this.node1 = node1;
-         this.node2 = node2;
-         this.weight = weight;
-      }
-   }
-
-    /*
-    (229, 229, 229) Zone non constructible = 0 Tile asset : snow-tile-hill
-    (255, 233, 127) Chemin = 1 Tile asset : snow-tile-straight/square
-    (255, 178, 127) intersection = 2 Tile asset : snow-tile-split/crossing
-    (0, 255, 33) Entree = 3 Tile asset : snow-tile-end
-    (255, 0, 0) Destination = 4 Tile asset : snow-tile-end-round
-    other Zone constructible = 5 Tile asset : snow-tile
-    */
 
    private Color32 colorEmpty = new Color32(229, 229, 229, 255);
    private Color32 colorRoad = new Color32(255, 233, 127, 255);
@@ -83,7 +58,7 @@ public class Map : MonoBehaviour
    private GameObject TileCollider;
 
    [SerializeField]
-   private GameObject TileEmpty; // visual of the tile
+   private GameObject TileEmpty; 
 
    [SerializeField]
    private GameObject TileRoad;
@@ -110,16 +85,9 @@ public class Map : MonoBehaviour
    private int height;
    private int width;
 
-   private Dictionary<coords, TileType> mapGraphNodes; // Dictionary that contains all nodes and their infos
-   private List<edge> mapGraphAdj; // List of weighted edges, weight being the length of the road between two nodes
+   private Graph graph;
 
 
-
-   /// <summary>
-   /// Given a coords, return weither it is inside the mapArray or not
-   /// </summary>
-   /// <param name="pos"></param>
-   /// <returns></returns>
    public bool IsInMap(coords pos)
    {
       if (pos.column < 0 || pos.row < 0 || this.mapArray.GetLength(0) <= pos.row || this.mapArray.GetLength(1) <= pos.column)
@@ -129,25 +97,15 @@ public class Map : MonoBehaviour
       return true;
    }
 
-   /// <summary>
-   /// convert a 3d position vector in coords for the mapArray (ignore the y axis)
-   /// </summary>
-   /// <param name="position"></param>
-   /// <returns></returns>
    public coords PositionToCoords(Vector3 position) 
    {
       return new coords((int)Math.Floor(position.z), (int)Math.Floor(position.x));
    }
 
    /// <summary>
-   /// mapArray is the 2DArray representation of a map
+   /// mapArray is the 2DArray representation of a map, access by coordss
    /// </summary>
    /// <returns></returns>
-   public TileType[,] GetMapArray()
-   {
-      return this.mapArray;
-   }
-
    public TileType GetMapArrayCoords(coords pos)
    {
       Debug.Assert(IsInMap(pos));
@@ -160,22 +118,9 @@ public class Map : MonoBehaviour
       this.mapArray[pos.row, pos.column] = type;
    }
 
-/// <summary>
-/// Dictionary that contains all nodes and their infos
-/// </summary>
-/// <returns></returns>
-   public Dictionary<coords, TileType> GetMapGraphNodes()
+   public Graph GetGraph()
    {
-      return this.mapGraphNodes;
-   }
-/// <summary>
-/// List of weighted edges, weight being the length of the road between two nodes
-/// </summary>
-/// <remarks>A node with no edges do not appear in this dictionnary</remarks>
-/// <returns></returns>
-   public List<edge> GetMapGraphAdj()
-   {
-      return this.mapGraphAdj;
+      return this.graph;
    }
 
 
@@ -189,8 +134,9 @@ public class Map : MonoBehaviour
       this.mapArray = LoadMapArray(mapDiskPath);
       this.height = mapArray.GetLength(0);
       this.width = mapArray.GetLength(1);
-      this.mapGraphNodes = CreateMapGraphNodes();
-      this.mapGraphAdj = CreateMapGraphAdj(mapGraphNodes);
+      this.graph = new();
+      this.graph.CreateNodes(mapArray);
+      this.graph.CreateMapGraphAdj(mapArray);
       CheckMap();
       LoadTiles();
 
@@ -256,111 +202,6 @@ public class Map : MonoBehaviour
 
       return mapArray;
    }
-
-   private Dictionary<coords, TileType> CreateMapGraphNodes()
-   {
-      Dictionary<coords, TileType> mapGraphNodes = new Dictionary<coords, TileType> {}; 
-      for (int row = 0; row < mapArray.GetLength(0); row++)
-      {
-         for (int column = 0; column < mapArray.GetLength(1); column++)
-         {
-            coords pos = new coords(row, column);
-            if (mapArray[row, column] == TileType.start)
-            {
-               mapGraphNodes[pos] = TileType.start;
-            }
-            else if (mapArray[row, column] == TileType.end)
-            {
-               mapGraphNodes[pos] = TileType.end;
-            }
-            else if (mapArray[row, column] == TileType.cross)
-            {
-               mapGraphNodes[pos] = TileType.cross;
-            }
-         }
-      }
-
-      return mapGraphNodes;
-   }
-
-
-   /// <summary>
-   /// This function creates the given graph of a mapArray
-   /// </summary>
-   /// <param name="mapArray"></param>
-   /// <returns></returns>
-   private List<edge> CreateMapGraphAdj(Dictionary<coords, TileType> mapGraphNodes)
-   { 
-
-      List<edge> mapGraphAdj = new();
-      bool OnAPath = false;
-      coords lastCrossPosition = new coords(-1,-1);
-      int weight = 0;
-
-      // Horizontal sweep
-      for (int row = 0; row < mapArray.GetLength(0); row++)
-      {
-         for (int column = 0; column < mapArray.GetLength(1); column++)
-         {
-            coords pos = new coords(row, column);
-            if (OnAPath && mapArray[row, column] == TileType.road)
-            {
-               weight++;
-            }
-            else if (OnAPath && (mapArray[row, column] == TileType.start || mapArray[row, column] == TileType.end || mapArray[row, column] == TileType.cross)) //Si on est 
-            {
-               mapGraphAdj.Add(new edge(lastCrossPosition, pos, weight));                  
-               mapGraphAdj.Add(new edge(pos, lastCrossPosition, weight));
-               weight = 0;
-            }
-            else{
-               OnAPath = false;
-            }
-
-            if (mapArray[row, column] == TileType.start || mapArray[row, column] == TileType.end || mapArray[row, column] == TileType.cross)
-            {
-               OnAPath = true;
-               lastCrossPosition = pos;
-            }
-         }
-         OnAPath = false;
-      }
-
-      weight = 0;
-      OnAPath = false;
-      // Vertical sweep
-      for (int column = 0; column < mapArray.GetLength(1); column++)
-      {
-         for (int row = 0; row < mapArray.GetLength(0); row++)
-         {
-            coords pos = new coords(row, column);
-            if (OnAPath && mapArray[row, column] == TileType.road)
-            {
-               weight++;
-            }
-            else if (OnAPath && (mapArray[row, column] == TileType.start || mapArray[row, column] == TileType.end || mapArray[row, column] == TileType.cross)) //Si on est 
-            {
-               mapGraphAdj.Add(new edge(lastCrossPosition, pos, weight));                  
-               mapGraphAdj.Add(new edge(pos, lastCrossPosition, weight));
-               weight = 0;
-            }
-            else{
-               OnAPath = false;
-            }
-
-            if (mapArray[row, column] == TileType.start || mapArray[row, column] == TileType.end || mapArray[row, column] == TileType.cross)
-            {
-               OnAPath = true;
-               lastCrossPosition = pos;
-            }
-         }  
-         OnAPath = false;
-      }
-   
-
-      return mapGraphAdj;
-   }
-
 
    /// <summary>
    /// Verifies the validity of a map
@@ -444,8 +285,6 @@ public class Map : MonoBehaviour
       }
       if (count == 3)
       {
-         Debug.Log("testest : " + pos);
-         Debug.Log(downNeighboor);
          if (downNeighboor == false)
          {
          return Quaternion.AngleAxis(0, Vector3.up);            
@@ -570,5 +409,173 @@ public class Map : MonoBehaviour
             }
          }
       }
+   }
+
+   public class Graph
+   {
+      private Dictionary<coords, TileType> mapGraphNodes; // Dictionary that contains all nodes and their infos
+      private List<edge> mapGraphAdj; // List of weighted edges, weight being the length of the road between two nodes
+      private List<node> nodes; // List that contains all nodes
+
+      public struct node 
+      {
+         public coords pos;
+         public TileType type;
+         public int distanceFromStart;
+         public coords nextNodeToStart;
+         public int distanceFromEnd;
+         public coords nextNodeToEnd;
+         public node(coords pos, TileType type, int distanceFromStart, coords nextNodeToStart, int distanceFromEnd, coords nextNodeToEnd)
+         {
+            this.pos = pos;
+            this.type = type;
+            this.distanceFromStart = distanceFromStart;
+            this.nextNodeToStart = nextNodeToStart;
+            this.distanceFromEnd = distanceFromEnd;
+            this.nextNodeToEnd = nextNodeToEnd;
+         }
+      }
+
+
+      /// <summary>
+      /// used for listing edges in mapArrayGraphAdj
+      /// </summary>
+      public struct edge 
+      {
+         public coords node1;
+         public coords node2;
+
+         public int weight;
+         public edge(coords node1, coords node2, int weight)
+         {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.weight = weight;
+         }
+      }
+
+   /// <summary>
+   /// Dictionary that contains all nodes and their infos
+   /// </summary>
+   /// <returns></returns>
+      public Dictionary<coords, TileType> GetMapGraphNodes()
+      {
+         return this.mapGraphNodes;
+      }
+   /// <summary>
+   /// List of weighted edges, weight being the length of the road between two nodes
+   /// </summary>
+   /// <remarks>A node with no edges do not appear in this dictionnary</remarks>
+   /// <returns></returns>
+      public List<edge> GetMapGraphAdj()
+      {
+         return this.mapGraphAdj;
+      }
+
+
+      public Dictionary<coords, TileType> CreateNodes(TileType[,] mapArray)
+      {
+         Dictionary<coords, TileType> mapGraphNodes = new Dictionary<coords, TileType> {}; 
+         for (int row = 0; row < mapArray.GetLength(0); row++)
+         {
+            for (int column = 0; column < mapArray.GetLength(1); column++)
+            {
+               coords pos = new coords(row, column);
+               if (mapArray[row, column] == TileType.start)
+               {
+                  mapGraphNodes[pos] = TileType.start;
+               }
+               else if (mapArray[row, column] == TileType.end)
+               {
+                  mapGraphNodes[pos] = TileType.end;
+               }
+               else if (mapArray[row, column] == TileType.cross)
+               {
+                  mapGraphNodes[pos] = TileType.cross;
+               }
+            }
+         }
+
+         return mapGraphNodes;
+      }
+
+
+      /// <summary>
+      /// This function creates the given graph of a mapArray
+      /// </summary>
+      /// <param name="mapArray"></param>
+      /// <returns></returns>
+      public List<edge> CreateMapGraphAdj(TileType[,] mapArray)
+      { 
+         Debug.Assert(this.mapGraphNodes != null);
+         List<edge> mapGraphAdj = new();
+         bool OnAPath = false;
+         coords lastCrossPosition = new coords(-1,-1);
+         int weight = 0;
+
+         // Horizontal sweep
+         for (int row = 0; row < mapArray.GetLength(0); row++)
+         {
+            for (int column = 0; column < mapArray.GetLength(1); column++)
+            {
+               coords pos = new coords(row, column);
+               if (OnAPath && mapArray[row, column] == TileType.road)
+               {
+                  weight++;
+               }
+               else if (OnAPath && (mapArray[row, column] == TileType.start || mapArray[row, column] == TileType.end || mapArray[row, column] == TileType.cross)) //Si on est 
+               {
+                  mapGraphAdj.Add(new edge(lastCrossPosition, pos, weight));                  
+                  mapGraphAdj.Add(new edge(pos, lastCrossPosition, weight));
+                  weight = 0;
+               }
+               else{
+                  OnAPath = false;
+               }
+
+               if (mapArray[row, column] == TileType.start || mapArray[row, column] == TileType.end || mapArray[row, column] == TileType.cross)
+               {
+                  OnAPath = true;
+                  lastCrossPosition = pos;
+               }
+            }
+            OnAPath = false;
+         }
+
+         weight = 0;
+         OnAPath = false;
+         // Vertical sweep
+         for (int column = 0; column < mapArray.GetLength(1); column++)
+         {
+            for (int row = 0; row < mapArray.GetLength(0); row++)
+            {
+               coords pos = new coords(row, column);
+               if (OnAPath && mapArray[row, column] == TileType.road)
+               {
+                  weight++;
+               }
+               else if (OnAPath && (mapArray[row, column] == TileType.start || mapArray[row, column] == TileType.end || mapArray[row, column] == TileType.cross)) //Si on est 
+               {
+                  mapGraphAdj.Add(new edge(lastCrossPosition, pos, weight));                  
+                  mapGraphAdj.Add(new edge(pos, lastCrossPosition, weight));
+                  weight = 0;
+               }
+               else{
+                  OnAPath = false;
+               }
+
+               if (mapArray[row, column] == TileType.start || mapArray[row, column] == TileType.end || mapArray[row, column] == TileType.cross)
+               {
+                  OnAPath = true;
+                  lastCrossPosition = pos;
+               }
+            }  
+            OnAPath = false;
+         }
+      
+
+         return mapGraphAdj;
+      }
+
    }
 }
