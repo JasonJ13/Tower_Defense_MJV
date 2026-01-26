a231f60435fab8731b28cef714336e73a7fa1a0c
Célia D'Agostini <celidago57@gmail.com>
2026-01-26 12:32:15 +0100

    les ennemis marchent à peu près (moon walk)

--- Assets/Script/Enemies/RobotEnemy.cs
+++ Assets/Script/Enemies/RobotEnemy.cs
@@ -1,7 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.PlayerSettings;
+using Debug = UnityEngine.Debug;
 
 public class RobotEnemy : MonoBehaviour
 {
@@ -10,29 +14,89 @@ public class RobotEnemy : MonoBehaviour
     [SerializeField] private int damage;
     [SerializeField] private float randomPath;
 
-    private Vector2 destination;
+    private Vector3 destination;
     private int currentHP;
 
     private Animator anim;
 
-    private EnemyGraph graph;
+    private List<Map.coords> destinations; 
+
+    private CharacterController characterController;
+
+    private Vector3 direction;
+    private float rotation;
+
+    private bool marching = false;
 
 
     private void Start()
     {
         anim = gameObject.GetComponent<Animator>();
         currentHP = maxHP;
+        characterController = gameObject.GetComponent<CharacterController>();
 
+        this.destinations = new List<Map.coords>() { new Map.coords(1, 5), new Map.coords(4,5) };
+        Debug.Log(destinations.Count);
+        if (destinations.Count > 0 )
+            ChangeDestination();
+
+        transform.position = Map.Instance.CoordsToPosition(new Map.coords(1, 1));
     }
 
-    
-
-    private void SetDestination(Vector2 destination)
+    private void Update()
     {
-        this.destination = destination;
+        if (destination !=null && marching)
+        {
+
+            var pos = transform.position;
+            
+            direction = destination - transform.position;
+            direction.y = 0;
+            var distance = direction.magnitude;
+            direction = direction / distance;
+            Debug.Log(direction);
+            Debug.Log(distance);
+
+           
+            if (distance<0.05)
+            {
+                transform.position = destination;
+                if (destinations.Count > 0)
+                {
+                    Debug.Log("change destination");
+                    ChangeDestination();
+                }
+                else
+                {
+                    anim.SetTrigger("Attack");
+                    Debug.Log("Fin du parcours");
+                }
+                
+            }
+            else
+            {
+                characterController.Move(direction * speed * Time.deltaTime);
+            }
+        }
+        
+
     }
 
 
+
+
+    private void ChangeDestination()
+    {
+        var new_coords = destinations.FirstOrDefault();
+        destinations.Remove(new_coords);
+        this.destination = Map.Instance.CoordsToPosition(new_coords);
+        Debug.Log(destination);
+        this.rotation = Vector2.Angle(new Vector2(transform.position.x, transform.position.z), new Vector2(destination.x, destination.z));
+        //Debug.Log(rotation);
+        
+
+
+    }
 
     public void TakeDamage(int damage)
     {
@@ -46,14 +110,17 @@ public class RobotEnemy : MonoBehaviour
     
     public void Die()
     {
-        anim.SetBool("Open_Anim", false);
+        anim.SetTrigger("Die");
 
     }
-    protected void DeathAnimFinished()
+    protected void DeathAnimFinished() //event de fin de l'anim de mort
     {
-        
         Debug.Log("mort");
-        Destroy(gameObject,0.5f);
+    }
+
+    protected void FinishedOpening() //event de fin de l'animation opening
+    {
+        marching = true;
     }
 
 
