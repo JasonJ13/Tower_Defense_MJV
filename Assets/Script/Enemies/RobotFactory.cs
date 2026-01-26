51f249ce6be0d19f3eb12c730d65285c834236ca
Célia D'Agostini <celidago57@gmail.com>
2026-01-26 20:21:31 +0100

    début factory

--- Assets/Script/Enemies/RobotFactory.cs
+++ Assets/Script/Enemies/RobotFactory.cs
@@ -2,6 +2,8 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class RobotFactory : MonoBehaviour
 {
     public static RobotFactory Instance { get; private set; }
@@ -15,11 +17,24 @@ public class RobotFactory : MonoBehaviour
         RobotFactory.Instance = this;
     }
 
+    [SerializeField] private GameObject standartRobot;
+    [SerializeField] private GameObject quickRobot;
+    [SerializeField] private GameObject bigRobot;
+    [SerializeField] private GameObject jesterRobot;
+
     public int level;
 
     public int waveIndex;
 
+    private Map.Graph graph;
+    private List<Map.coords> starts;
     private List<RobotEnemy> robots;
+
+    private void Start()
+    {
+        graph = Map.Instance.GetGraph();
+        starts = graph.GetAllStart();
+    }
 
     public RobotEnemy FindRobotOnTile(Map.coords coord)
     {
@@ -33,4 +48,15 @@ public class RobotFactory : MonoBehaviour
         }
         return null;
     }
+
+    private void SpawnRobot(GameObject robot)
+    {
+        int rIndex = Random.Range(0, starts.Count);
+        var start = this.starts[rIndex];
+
+        GameObject enemy = Instantiate(robot, Map.Instance.CoordsToPosition(start), Quaternion.identity);
+        robots.Add(enemy.GetComponent<RobotEnemy>());
+    }
+
+    
 }
